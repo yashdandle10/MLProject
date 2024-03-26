a53910b952f85a574d8a4b023134dd30abf27bc4
yashdandle10 <yash.dandle@gmail.com>
2024-03-27 00:46:40 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPER_E_DOT = '-e .'
+def get_requirements(file_path:str) -> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPER_E_DOT in requirements:
+            requirements.remove(HYPER_E_DOT)
+    return requirements    
+
+
+
+setup(
+
+    name = 'MLProject', 
+    version = '0.0.1',
+    author = 'yash',
+    author_email = 'yash.dandle@gmail.com',
+    packages = find_packages(), 
+    install_requires = ['pandas', 'numpy', 'seaborn'] 
+
+)
+
